7ba7980e4710e56da1de4d3fcaf331e7599c47be
Saurav Chauhan <sauravchauhan2k@gmail.com>
2021-09-07 14:33:50 +0530

    fix message display, it will show message of one room only

--- ChatServicePlayground/chat/consumers.py
+++ ChatServicePlayground/chat/consumers.py
@@ -9,7 +9,7 @@ User = get_user_model()
 class ChatConsumer(WebsocketConsumer):
 
     def fetch_messages(self, data):
-        messages = Message.last_10_messages(self,)
+        messages = Message.last_10_messages(self,data['room_id'])
         content = {
             'command': 'messages',
             'messages': self.messages_to_json(messages)
@@ -21,7 +21,9 @@ class ChatConsumer(WebsocketConsumer):
         author_user = User.objects.filter(username=author)[0]
         message = Message.objects.create(
             author=author_user, 
-            content=data['message'])
+            content=data['message'],
+            room_name=data['room_id']
+            )
         content = {
             'command': 'new_message',
             'message': self.message_to_json(message)
--- ChatServicePlayground/chat/models.py
+++ ChatServicePlayground/chat/models.py
@@ -8,9 +8,10 @@ class Message(models.Model):
     author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-
+    room_name = models.CharField(max_length=255,blank=True,null=True)
+    
     def __str__(self):
         return self.author.username
 
-    def last_10_messages(self):
-        return Message.objects.order_by('-timestamp').all()[:10:-1]
+    def last_10_messages(self, id='lobby'):
+        return Message.objects.order_by('-timestamp').filter(room_name=id)[::-1]
